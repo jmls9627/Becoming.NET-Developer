f26cf43fb1bddde5f056e5ba373f3b92f87cabb3
unknown <jmls9627@gmail.com>
2020-10-06 16:10:45 -0400

    C# basics Debugging

---
+++ Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = AddNumber(30, 40);
+            Console.WriteLine("The number is :" + result);
+            Console.WriteLine(AddNumber(20, 40));
+            Console.ReadKey();
+        }
+
+        private static int AddNumber(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
