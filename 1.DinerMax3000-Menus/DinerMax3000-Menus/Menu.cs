2a317b0a1ced386d7adf800dc37b4e4d0b2bb4dc
unknown <jmls9627@gmail.com>
2020-10-07 13:35:33 -0400

    Example: DinerMax3000-clases

---
+++ 1.DinerMax3000-Menus/DinerMax3000-Menus/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000_Menus
+{
+    class Menu
+    {
+        public string Name;
+        public List<MenuItem> items;
+
+        public Menu()
+        {
+            items = new List<MenuItem>();
+        }
+
+    }
+}
---
+++ 1.DinerMax3000-Menus/DinerMax3000-Menus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000_Menus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu summerMenu = new Menu();
+            summerMenu.Name = "Summer Menu";
+            MenuItem salmon = new MenuItem();
+            salmon.Title = "Salmon";
+            salmon.Description = "Salmon from the Alpes";
+            salmon.Price = 25.50;
+            summerMenu.items.Add(salmon);
+
+        }
+    }
+}
