91185cf7474b015dee244e64a22be06ae0f0d867
unknown <jmls9627@gmail.com>
2020-10-12 13:51:48 -0400

    Example: DinerMax3000-Inserting into Multiple Tables

--- 1.DinerMax3000-Menus/DinerMax3000BusinessLayer/DS_DinerMax3000.Designer.cs
+++ 1.DinerMax3000-Menus/DinerMax3000BusinessLayer/DS_DinerMax3000.Designer.cs
@@ -2501,7 +2501,7 @@ SELECT Id, Name, MenuType, Disclaimer, HospitalDirections FROM Menu WHERE (Id = 
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "15.0.0.0")]
         private void InitCommandCollection() {
-            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[2];
+            this._commandCollection = new global::System.Data.SqlClient.SqlCommand[3];
             this._commandCollection[0] = new global::System.Data.SqlClient.SqlCommand();
             this._commandCollection[0].Connection = this.Connection;
             this._commandCollection[0].CommandText = "SELECT Id, Name, Description, Price FROM dbo.MenuItem";
@@ -2514,6 +2514,16 @@ SELECT Id, Name, MenuType, Disclaimer, HospitalDirections FROM Menu WHERE (Id = 
                 "Id)";
             this._commandCollection[1].CommandType = global::System.Data.CommandType.Text;
             this._commandCollection[1].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@MenuId", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "MenuId", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2] = new global::System.Data.SqlClient.SqlCommand();
+            this._commandCollection[2].Connection = this.Connection;
+            this._commandCollection[2].CommandText = @"INSERT INTO [dbo].[MenuItem] ([Name], [Description], [Price]) VALUES (@Name, @Description, @Price);
+insert into Menu_MenuItem (MenuId,MenuItemId) values (@MenuId,SCOPE_IDENTITY());
+SELECT Id, Name, Description, Price FROM MenuItem WHERE (Id = SCOPE_IDENTITY())";
+            this._commandCollection[2].CommandType = global::System.Data.CommandType.Text;
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Name", global::System.Data.SqlDbType.VarChar, 100, global::System.Data.ParameterDirection.Input, 0, 0, "Name", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Description", global::System.Data.SqlDbType.Text, 2147483647, global::System.Data.ParameterDirection.Input, 0, 0, "Description", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@Price", global::System.Data.SqlDbType.Float, 8, global::System.Data.ParameterDirection.Input, 0, 0, "Price", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
+            this._commandCollection[2].Parameters.Add(new global::System.Data.SqlClient.SqlParameter("@MenuId", global::System.Data.SqlDbType.Int, 4, global::System.Data.ParameterDirection.Input, 0, 0, "MenuId", global::System.Data.DataRowVersion.Current, false, null, "", "", ""));
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
@@ -2693,6 +2703,43 @@ SELECT Id, Name, MenuType, Disclaimer, HospitalDirections FROM Menu WHERE (Id = 
         [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Update, true)]
         public virtual int Update(string Name, string Description, double Price, int Original_Id, string Original_Name, double Original_Price) {
             return this.Update(Name, Description, Price, Original_Id, Original_Name, Original_Price, Original_Id);
+        }
+        
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "15.0.0.0")]
+        [global::System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")]
+        [global::System.ComponentModel.DataObjectMethodAttribute(global::System.ComponentModel.DataObjectMethodType.Insert, false)]
+        public virtual int InsertNewMenuItem(string Name, string Description, double Price, int MenuId) {
+            global::System.Data.SqlClient.SqlCommand command = this.CommandCollection[2];
+            if ((Name == null)) {
+                throw new global::System.ArgumentNullException("Name");
+            }
+            else {
+                command.Parameters[0].Value = ((string)(Name));
+            }
+            if ((Description == null)) {
+                throw new global::System.ArgumentNullException("Description");
+            }
+            else {
+                command.Parameters[1].Value = ((string)(Description));
+            }
+            command.Parameters[2].Value = ((double)(Price));
+            command.Parameters[3].Value = ((int)(MenuId));
+            global::System.Data.ConnectionState previousConnectionState = command.Connection.State;
+            if (((command.Connection.State & global::System.Data.ConnectionState.Open) 
+                        != global::System.Data.ConnectionState.Open)) {
+                command.Connection.Open();
+            }
+            int returnValue;
+            try {
+                returnValue = command.ExecuteNonQuery();
+            }
+            finally {
+                if ((previousConnectionState == global::System.Data.ConnectionState.Closed)) {
+                    command.Connection.Close();
+                }
+            }
+            return returnValue;
         }
     }
     
--- 1.DinerMax3000-Menus/DinerMax3000BusinessLayer/Menu.cs
+++ 1.DinerMax3000-Menus/DinerMax3000BusinessLayer/Menu.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DinerMax3000.Business.DS_DinerMax3000TableAdapters;
+
 
 namespace DinerMax3000.Business
 {
@@ -23,9 +25,34 @@ namespace DinerMax3000.Business
             item.Description = Description;
             item.Price = Price;
             items.Add(item);
-     
+        }
 
+        public static List<Menu> GetAllMenus()
+        {
+            MenuTableAdapter TaMenu = new MenuTableAdapter();
+            MenuItemTableAdapter TaMenuItem = new MenuItemTableAdapter();
+
+            var dtMenu = TaMenu.GetData();
+            List<Menu> allMenus = new List<Menu>();
+
+            foreach(DS_DinerMax3000.MenuRow menuRow in dtMenu.Rows)
+            {
+                Menu currentMenu = new Menu();
+                currentMenu.Name = menuRow.Name;
+                allMenus.Add(currentMenu);
+
+                var dtMenuItem = TaMenuItem.GetMenuItemsByMenuId(menuRow.Id);
+                foreach(DS_DinerMax3000.MenuItemRow menuItemRow in dtMenuItem.Rows)
+                {
+                    currentMenu.AddMenuItems(menuItemRow.Name, menuItemRow.Description, menuItemRow.Price);
+                }
+            }
+            return allMenus;
 
         }
+
+
+
     }
+
 }
